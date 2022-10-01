17bb8261b0deaedd4de9bd3bc7be563b29885094
Ishan <ishanwijes@gmail.com>
2022-10-02 06:36:06 +1100

    useQuery basics completed

--- 4 - useQuery/client/src/components/App.js
+++ 4 - useQuery/client/src/components/App.js
@@ -1,13 +1,37 @@
 
+// so here is what we've done, we have a client and a server folder
+// the client folder handles the frontend code and the server folder handles the backend code, when you run the command node index.js in the server folder, an Apollo Server is started on port 4000 (by default)
+// just like in folder 3 - Apollo Server, all that's happening in the backend is that we have a typeDefs file and a resolvers file. We also have a FakeData.js file that acts like the database for learning purposes, that file is importing and used in resolvers.js. All of these files come together in index.js where the apollo server is actually created
+// you need to start the application on server (node index.js) and start the application on client (npm start)
+
+// import react
 import React from "react";
+
+// these are things you need to import to make apollo client work
+import { ApolloClient, InMemoryCache, ApolloProvider, useQuery } from '@apollo/client'
+import { DisplayData } from "./DisplayData";
 
 
 
 export const App = () => {
-    
+    // here we create a variable that stores a new instance of the ApolloClient constructor function
+    const client = new ApolloClient({
+        // this is just telling apollo client to save data in cache if need be
+        cache: new InMemoryCache(),
+        // the uri is just a url to where the graphql api is running
+        // remember how we opened up graphql on port 4000 by default with the endpoint /graphql, that is what this is referring too
+        uri: "http://localhost:4000/graphql"
+    })
+
     return (
-        <div>
-            hi there
-        </div>
+        // now for the returned JSX, wrap everything in <ApolloProvider></ApolloProvider> and add a client prop which takes in the client variable we created above
+        <ApolloProvider client={client}>
+            <div>
+                <h1>List of Users</h1>
+                
+                {/* this is the component which will render the data we query from the backend */}
+                <DisplayData />
+            </div>
+        </ApolloProvider>
     )
 }
---
+++ 4 - useQuery/client/src/components/DisplayData.js
@@ -0,0 +1,65 @@
+
+import React from "react";
+
+// import useQuery and gql
+import { useQuery, gql } from '@apollo/client'
+
+// now that we've done all that, we can finally query the data FROM THE BACKEND, in the frontend
+// to do that, we use useQuery(), which is just a react hook like useEffect() or useState()
+// useQuery() fetches data from an API whenever a component renders
+
+// to write a graphql query here in the js code, we use a gql statement
+// variable name is usually written in this format
+const QUERY_ALL_USERS = gql`
+    query {
+        users {
+            id
+            name
+            username
+        }
+    }
+`
+
+export const DisplayData = () => {
+    
+    // this is how you use useQuery, you put the query gql statement as the argument and you destructure the data out of it
+    // in this case, data is an object, and one of the properties of that object is an array of the information we actually want (data.users)
+    // we also destructure loading and error
+    const {data, loading, error} = useQuery(QUERY_ALL_USERS)
+
+    // this is really cool, because querying data from a database in the backend is an asynchronous task, the data object won't immediately be available, while that data object is being retrieved, the loading variable is TRUE
+    // so while the data is being retrieved you can return JSX for a loading screen
+    // set it up using an if statement
+    if(loading) {
+        return (
+            <div>
+                Loading Screen JSX
+            </div>
+        )
+    }
+
+    // if the error variable is true, you can return JSX for an error screen
+    if(error) {
+        return (
+            <div>
+                Error Screen JSX
+            </div>
+        )
+    }
+
+    // when loading is false and error is false, the final JSX that should be returned by this component would be the JSX we initially wanted to be returned
+    return (
+        <div>
+            {/* remember, you only want to display this data if data currently exists, meaning data is truthy because it has been retrieved from the backend */}
+            {data && data.users.map(object => {
+                return (
+                    <div key={object.id}>
+                        <h2>{object.id}</h2>
+                        <h3>{object.name}</h3>
+                        <h3>{object.username}</h3>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
